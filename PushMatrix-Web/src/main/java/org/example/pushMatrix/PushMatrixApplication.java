710c560db1b5c28d6c6ebb0f65244f8c8cd88478
泽 <3408579908@qq.com>
2024-07-30 22:20:29 +0800

    发送短信接口测试

---
+++ PushMatrix-Web/src/main/java/org/example/pushMatrix/PushMatrixApplication.java
@@ -0,0 +1,36 @@
+package org.example.pushMatrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashSet;
+@SpringBootApplication
+@RestController
+public class PushMatrixApplication {
+@Autowired
+private TencentSmsScript tencentSmsScript;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushMatrixApplication.class, args);
+    }
+    /**
+     * @param phone 手机号
+     * @return
+     */
+    @GetMapping("/sendSms")
+    public String sendSms(String phone,String content) {
+
+        /**
+         * 这里的content指的是模板占位符的参数值
+         */
+        SmsParam smsParam = SmsParam.builder()
+                .phones(new HashSet<>(Arrays.asList(phone)))
+                .content(content)
+                .build();
+        System.out.println();
+        return tencentSmsScript.send(smsParam);
+    }
+    }
---
+++ PushMatrix-Web/src/test/java/com/example/pushmatrixweb/PushMatrixWebApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.pushmatrixweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushMatrixWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(1);
+    }
+
+}
