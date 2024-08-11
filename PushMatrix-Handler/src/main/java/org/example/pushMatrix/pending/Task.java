afeb6d6cd40b2795994653cf9557ae04e5bd687e
泽 <3408579908@qq.com>
2024-08-11 15:31:42 +0800

    添加丢弃消息功能

--- PushMatrix-Handler/src/main/java/org/example/pushMatrix/pending/Task.java
+++ PushMatrix-Handler/src/main/java/org/example/pushMatrix/pending/Task.java
@@ -3,6 +3,7 @@ import com.tencentcloudapi.common.exception.TencentCloudSDKException;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
+import org.example.pushMatrix.discard.DiscardMessageService;
 import org.example.pushMatrix.domain.TaskInfo;
 import org.example.pushMatrix.handler.HandlerHolder;
 import org.example.pushMatrix.deduplication.DeduplicationRuleService;
@@ -27,11 +28,15 @@ public class Task implements Runnable{
     private TaskInfo taskInfo;
     @Autowired
     private DeduplicationRuleService deduplicationRuleService;
+    @Autowired
+    private DiscardMessageService discardMessageService;
 
     @Override
     public void run() {
         // 0. 丢弃消息
-
+        if (discardMessageService.isDiscard(taskInfo)) {
+            return;
+        }
         // 1.平台通用去重
         deduplicationRuleService.duplication(taskInfo);
 
